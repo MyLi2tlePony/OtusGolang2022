ec861a860c1a92bab2a19d893e20080ea5cf5ac5
MyLi2tlePony <shabandrew@mail.ru>
2022-06-26 21:39:45 +0300

    test push

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,33 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	var out Out
+	bi := make(Bi)
+
+	for index, stage := range stages {
+		if index == 0 {
+			out = stage(bi)
+		} else {
+			out = stage(out)
+		}
+	}
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				close(bi)
+				return
+			case value, ok := <-in:
+				if ok {
+					bi <- value
+				} else {
+					close(bi)
+					return
+				}
+			}
+		}
+	}()
+
+	return out
 }
