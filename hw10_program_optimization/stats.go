6670be4c06bc5e9d04c1e5d3efde989b646f9255
MyLi2tlePony <shabandrew@mail.ru>
2022-08-04 20:31:06 +0300

    Алгоритм сделан без горутин

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -3,11 +3,10 @@ package hw10programoptimization
 import (
 	"bufio"
 	"fmt"
+	"github.com/valyala/fastjson"
 	"io"
 	"regexp"
 	"strings"
-
-	"github.com/valyala/fastjson"
 )
 
 type User struct {
@@ -27,38 +26,21 @@ type (
 var regexpForDomain = regexp.MustCompile(`[^@]+$`)
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	email, err := getUsersEmails(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-
-	return countDomains(email, domain)
-}
-
-func getUsersEmails(r io.Reader) (<-chan string, error) {
-	emails := make(chan string, 1000)
-	scanner := bufio.NewScanner(r)
-
-	go func() {
-		defer close(emails)
-
-		for i := 0; scanner.Scan(); i++ {
-			emails <- fastjson.GetString(scanner.Bytes(), "Email")
-		}
-	}()
-
-	return emails, scanner.Err()
-}
-
-func countDomains(emails <-chan string, domainPart string) (DomainStat, error) {
+	regexpForEmail := regexp.MustCompile(`\.` + domain)
 	result := make(DomainStat)
-	regexpForEmail := regexp.MustCompile(`\.` + domainPart)
+	scan := bufio.NewScanner(r)
 
-	for email := range emails {
+	for i := 0; scan.Scan(); i++ {
+		email := fastjson.GetString(scan.Bytes(), "Email")
+
 		if regexpForEmail.MatchString(email) {
 			domain := strings.ToLower(regexpForDomain.FindString(email))
 			result[domain]++
 		}
+	}
+
+	if err := scan.Err(); err != nil {
+		return nil, fmt.Errorf("scanner error: %w", err)
 	}
 
 	return result, nil
